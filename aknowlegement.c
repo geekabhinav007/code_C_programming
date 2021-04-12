f7ce8e00f629e681d5abdb3cd39127f5b52720f5
Kumar Abhinav <76863411+geekabhinav007@users.noreply.github.com>
2021-04-12 14:14:34 +0530

    Delete aknowlegement.c

--- aknowlegement.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-
-{
-    printf("\n\n\t\t\t\t\tAcknowledgment");
-    printf("\n\t\tI would like to express my special thanks of gratitude to my PPS teacher H.S.RAI \n\t\twho gave me a golden opportunity to do this project. it gave me an immense pleasure\n\t\twhile doing this project because it was not just a project but , a source to learn\n\t\tnot just about chapter i came to know about so many new things I am really thankful\n\t\tto them. \n\n");
-    return 0;
-}
