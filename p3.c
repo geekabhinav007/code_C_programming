092b8591f3fca1e7525dedf8099a0cc7a3d5f768
Kumar Abhinav <76863411+geekabhinav007@users.noreply.github.com>
2021-04-12 14:15:29 +0530

    Delete p3.c

--- p3.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("\nName:-Kumar Abhinav\nBranch:-ECE\nClass Roll No.:-2017105\nHome Town:-Muzaffarpur\nPassion:-Ethical Hacking\n\n");
-    
-    return 0;
-}
